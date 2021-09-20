60f3dff12b472186ebc6e36945739569bb6e230b
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-09-20 04:34:06 -0300

    Criado e testado a API REST

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost:27017/todo', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useMongoClient: true
 })
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -5,7 +5,10 @@ const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 server.listen(port, function (){
     console.log('Back-End is running on port' + port)
 })
+
+module.exports = server
